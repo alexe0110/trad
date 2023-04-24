26125d132b5d7c01a621b03455ab931485345748
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2023-04-24 21:05:36 +0500

    add post

--- main.py
+++ main.py
@@ -27,6 +27,13 @@ def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
 
+@app.post("/user/{user_id}")
+def post_change_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "new name": current_user}
+
+
 @app.get("/trades")
 def get_trades(limit: int = 1, offset: int = 0):
     return fake_trades[offset:][:limit]
--- makefile
+++ makefile
@@ -3,6 +3,6 @@ PYTHON_VERSION := 3.11
 
 init:
 	python$(PYTHON_VERSION) -m venv $(VENV)
-	# $(VENV)/bin/python -m pip install --upgrade pip
-	# $(VENV)/bin/python -m pip install poetry
-	# $(VENV)/bin/poetry install
+	$(VENV)/bin/python -m pip install --upgrade pip
+	$(VENV)/bin/python -m pip install poetry
+	$(VENV)/bin/poetry install
