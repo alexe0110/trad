5c5671c1ea4cbee325d56f9ea89cb28053197099
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2023-05-08 20:08:56 +0500

    fixiki

--- main.py
+++ main.py
@@ -4,26 +4,12 @@ from fastapi import FastAPI
 from http import HTTPStatus
 
 from starlette.responses import JSONResponse
-
+from data import USERS, TRADES
 from models import Trade, User
 
 app = FastAPI(
     title="Trading App"
 )
-
-fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
-    {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"},
-    {"id": 4, "role": "trader", "name": "Homer", "degree": [
-        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-    ]},
-]
-
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
-]
 
 
 @app.get("/")
@@ -38,22 +24,22 @@ def hello(name: str):
 
 @app.get("/user/{user_id}", response_model=list[User])
 def get_user(user_id: int):
-    return [user for user in fake_users if user.get("id") == user_id]
+    return [user for user in USERS if user.get("id") == user_id]
 
 
 @app.post("/user/{user_id}")
 def post_change_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
+    current_user = list(filter(lambda user: user.get("id") == user_id, USERS))[0]
     current_user["name"] = new_name
     return {"status": HTTPStatus.OK, "new name": current_user}
 
 
 @app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
+def get_trades():
+    return TRADES
 
 
 @app.post("/trades")
 def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {"status": HTTPStatus.OK, "data": fake_trades}
+    TRADES.extend(trades)
+    return {"status": HTTPStatus.OK, "data": TRADES}
