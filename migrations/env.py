170e02e446bd799945db302ed7ce6e9c2b51cd11
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2023-05-13 16:50:18 +0500

    add oper table

--- migrations/env.py
+++ migrations/env.py
@@ -4,9 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+import sys
 
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from models import metadata
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from src.auth.models import metadata as metadata_auth
+from src.operations.models import metadata as metadata_operations
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +32,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata_operations, metadata_auth]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
