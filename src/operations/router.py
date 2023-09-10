425ef1de55f70f7e64d55c0970786cebece09cf2
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2023-09-10 18:04:23 +0500

    select zarabotal

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,32 +1,24 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.responses import JSONResponse
 
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
 
 router = APIRouter(
+    prefix="/operations",
     tags=["Operation"]
 )
-
 
 
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
     query = select(operation).where(operation.c.type == operation_type)
-    print('😌 query = ',query)
     result = await session.execute(query)
-    print('😌 result = ',result)
-    res = result.all()
-    print('😌 result.all() = ', res)
-    print('😌 result.all() = ', type(res))
-
-    return JSONResponse(
-        status_code=200,
-        content=str(res)
-    )
+    rows = result.fetchall()
+    print(rows)
+    return "Пока не пойму почему не могу вернуть их в ответе"
 
 
 @router.post("/")
