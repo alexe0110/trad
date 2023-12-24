4b15a3dbe7b38a8e05e0392a8f81ee118156ebfb
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2023-12-24 15:17:33 +0500

    search

--- src/pages/router.py
+++ src/pages/router.py
@@ -12,3 +12,8 @@ templates = Jinja2Templates(directory='src/templates')
 @router.get('/base')
 def get_base_page(request: Request):
     return templates.TemplateResponse('base.html', {'request': request})
+
+
+@router.get('/search')
+def search_operations(request: Request):
+    return templates.TemplateResponse('search.html', {'request': request})
